2cb247ea337195b480bbfcd8fb1049ef26c89617
NURASYL <nurasylabdykadyr40@gmail.com>
2026-08-09 12:11:03 +0500

    Auto create users table

--- server/index.js
+++ server/index.js
@@ -15,6 +15,17 @@ app.use(express.json());
 
 app.use('/api', apiRoutes);
 
+db.query(`
+  CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+  )
+`).then(() => console.log("Таблица users проверена/создана")).catch(err => console.error(err));
+
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Сервер запущен на порту ${PORT}`);
